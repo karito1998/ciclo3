04879082db663b79727aa8555023a58b6f357508
Karol Arciniegas <karol.arciniegas@mi.com.co>
2020-12-16 10:44:11 -0500

    seccion4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
   <div id="app">
+    <!--Seccion 4-->
+    <div class="container-fluid">
+      <div class="row justify-content-center mb-5">
+        <div class= "col mt-5" v-for="(miembro, index) of Team" :key = "index">
+          <team-card :member="miembro"></team-card>
+
+        </div>
+       </div>
+       </div> 
   </div>
 </template>
 
 <script>
+import TeamCard from './components/TeamCard.vue'
 
 export default {
   name: 'App',
+  components: { TeamCard },
+  data(){
+    return{
+    Team:[
+      {
+      codigo : 1,
+      nombre : 'Karol Arciniegas',
+      descripcion : 'Ingeniera Electronica egresada de la universidad ECCI en Bogota, amante de los gatos.',
+      rol: 'Analista de Software',
+      image : './public/imagenes/KarolGisselleArciniegasMartinez.png'
+      },
+      {
+      codigo : 2,
+      nombre : 'Andres Sabogal',
+      descripcion : 'Ingeniera Electronica egresada de la universidad ECCI en Bogota, amante de los gatos.',
+      rol: ' Desarrollador Backend',
+      image : './public/imagenes/AndresSabogal.png'
+      },
+      {
+      codigo : 3,
+      nombre : 'Deivier Zaraza',
+      descripcion : 'Ingeniera Electronica egresada de la universidad ECCI en Bogota, amante de los gatos.',
+      rol: 'Arquitecto de software',
+      image : './public/imagenes/DeivierZaraza.jpeg'
+      },
+    ]
+    }
+  }
+  
 }
 </script>
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-
+    <div>    
+        <div class = "card text-white bg-dark">
+            <div class= "d-flex justify-content-center p-2">
+                <img :src="member.image" alt="participante" height="250" width="250">
+            </div>
+        <div class = "card-body">
+            <h5 class = "card-title"> {{member.nombre}} </h5>
+            <p class = "card-text"> {{member.descripcion}} </p>
+        </div>
+        <div class = "card-footer">
+            <small class="text-muted"> {{member.rol}}</small>
+        </div>
     </div>
+</div>
+    
 </template>
 
 <script>
